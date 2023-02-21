23f4c269099fba918929108a3c2b8e0ee2bd6cee
Wang <wangqin0@outlook.com>
2023-02-20 23:44:04 -0800

    User auth & login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,88 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Auth route
 // @access  Public
-router.get('/', (req, res) => res.send('auth route'));
+// Note: just add `auth` to argument list will make the routes protected
+router.get('/', auth, async (req, res) => {
+  try {
+    // do not send the password back
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+    console.log("User authed");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post('/', [
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password is required').exists(),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
+
+  const {email, password} = req.body;
+
+  try {
+    // see if exists
+    let user = await User.findOne({email});
+
+    if (!user) {
+      console.error('User not exists');
+      return res.status(400).json({errors: [{msg: 'User not exists'}]});
+    }
+
+    // got user, make sure password matches
+    // first password from req, second from the database
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ errors: [{ msg: 'Wrong credential' }]});
+    }
+
+    // return jsonwebtoken
+    const payload = {
+      user: {
+        id: user.id,
+      }
+    };
+
+    console.log('payload: ', payload);
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      {expiresIn: 3600},
+      (err, token) => {
+        if (err) {
+          throw err;
+        }
+        res.json({token});
+      }
+    );
+
+    console.log('User logged in');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error.');
+  }
+
+});
 
 module.exports = router;
