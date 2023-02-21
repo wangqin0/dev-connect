fcd5270cb7ecbba7655be210f59da59bd1f5bc6d
Wang <wangqin0@outlook.com>
2023-02-20 16:57:53 -0800

    Basic routing.

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 5000;
 
 connectDb();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
