36e18a73d3da5561c964da20612b4406df1bd967
Wang <wangqin0@outlook.com>
2023-02-22 14:46:42 -0800

    Put/Delete experience.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,4 +154,81 @@ router.delete('/', auth, async (req, res) => {
 });
 
 
+// @route   DELETE api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put('/experience', [auth, [
+  check('title', 'title is required').not().isEmpty(),
+  check('company', 'company is required').not().isEmpty(),
+  check('from', 'from is required').not().isEmpty(),
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  } = req.body;
+
+  const newExp = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description,
+  };
+
+  console.log(newExp);
+
+  try {
+    const profile = await Profile.findOne({ userId: req.user.id });
+
+    console.log(req.user.id);
+    console.log(profile);
+
+    profile.experience.unshift(newExp);
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+
+// @route   DELETE api/profile/experience
+// @desc    Delete profile experience
+// @access  Private
+router.delete('/experience/:exp_id', [auth], async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ userId: req.user.id });
+
+    console.log(profile);
+
+    // get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+
 module.exports = router;
