cc845c8f2bebab9ce9d2307c1253416bb48e5f7b
Wang <wangqin0@outlook.com>
2023-02-23 20:20:25 -0800

    Create and delete a comment.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -85,7 +85,7 @@ router.delete('/:post_id', auth, async (req, res) => {
 
     // check ownership
     if (post.userId.toString() !== req.user.id) {
-      return res.status(401).json({msg: 'User not authrized'});
+      return res.status(401).json({msg: 'User not authorized'});
     }
 
     await post.remove();
@@ -155,6 +155,83 @@ router.put('/unlike/:post_id', auth, async (req, res) => {
     res.json(post.likes);
   } catch (err) {
     console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+
+// @route   POST api/posts/comment/:post_id
+// @desc    Create a comment on a post
+// @access  Private
+router.post('/comment/:post_id', [auth, [
+  check('text', 'text is required').not().isEmpty(),
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
+
+  try {
+    const post = await Post.findById(req.params.post_id);
+    const user = await User.findById(req.user.id).select('-password');
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      userId: req.user.id,
+    };
+
+    post.comments.push(newComment);
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+
+// @route   DELETE api/posts/comment/:post_id/:comment_id
+// @desc    Delete a comment from a post by their ids
+// @access  Private
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({msg: 'Post not found'});
+    }
+
+    console.log(post.userId);
+    console.log(req.user.id);
+    console.log(post.comments);
+
+    const removeIndex = post.comments
+      .map(comment => comment.id.toString())
+      .indexOf(req.params.comment_id);
+    console.log(removeIndex);
+    if (removeIndex < 0 || removeIndex >= post.comments.length) {
+      return res.status(400).json({msg: 'the comment on that post is not found'});
+    }
+
+    // check ownership
+    if (post.userId.toString() !== req.user.id
+      && post.comments[removeIndex].userId.toString() !== req.user.id) {
+      return res.status(400).json({msg: 'You can only delete your comments or comments in your posts'});
+    }
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({msg: 'Post not found'});
+    }
     res.status(500).send('Server error');
   }
 });
