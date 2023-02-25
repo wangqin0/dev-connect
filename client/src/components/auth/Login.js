1a2897a6a6381c2527967daf6c624de32f34ad46
Wang <wangqin0@outlook.com>
2023-02-25 00:10:04 -0800

    Login.js & Register.js

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
-const Login = () => (
-  <div className="container">Login</div>
-);
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log('Login success');
+  };
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign in to your account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={formData.email}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={formData.password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login"/>
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="login">Sign Up</Link>
+      </p>
+    </section>
+  );
+};
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,99 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (formData.password !== formData.password2) {
+      console.error('passwords do not match');
+    } else {
+      console.log(formData);
+
+      // To access backend '/api/users'
+      // const {name, email, password} = formData;
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password,
+      // };
+      //
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //   };
+      //
+      //   const body = JSON.stringify(newUser);
+      //   const res = await axios.post('/api/users', body, config);
+      //   console.log(res.data);
+      // } catch (err) {
+      //   console.error(err.response.data);
+      // }
+    }
+  };
+
   return (
-    <div className="container">Register</div>
+    <section className="container">
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={formData.name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={formData.email}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={formData.password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="6"
+            value={formData.password2}
+            onChange={e => onChange(e)}
+          />
+        < /div>
+        <input type="submit" className="btn btn-primary" value="Register"/>
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="login">Sign In</Link>
+      </p>
+    </section>
   );
 };
 
